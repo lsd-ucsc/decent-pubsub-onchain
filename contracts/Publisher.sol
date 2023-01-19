0515eedef3b7eea33bfa5423690368c3e0b0b610
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2023-01-19 14:15:35 -0800

    adding code for subscriber reimbursement

--- contracts/Publisher.sol
+++ contracts/Publisher.sol
@@ -13,8 +13,22 @@ contract Publisher {
 
     Subscriber[] subscribers;
 
-    function subscribe(uint balance) external {
+    constructor(uint256 deposit) payable { 
+        require(msg.value == deposit); // deposit is in Wei
+        require(msg.value > 0);
+        deposit = msg.value;
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+
+
+    function subscribe(address payable user, uint balance) external {
+        uint gas_cost = gasleft();
         subscribers.push(Subscriber(msg.sender, balance));
+        user.transfer(gas_cost);        
     }
 
     function notify(address member) external {
@@ -24,5 +38,5 @@ contract Publisher {
             }
         }
     }
-}
 
+}
