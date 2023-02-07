01465aeb083f30eddfaec350bdeb02730938a88b
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2023-02-06 16:19:24 -0800

    Working publisher and small edits to subscriber so that sent wei isn't hardcoded

--- contracts/Subscriber.sol
+++ contracts/Subscriber.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.17;
 
 interface InterfacePublisher {
-    function subscribe(address user, uint balance) external;
+    function subscribe(address user, uint balance) external payable;
     function notify(address member) external;
 }
 
 contract Subscriber {
     address publisher;
-    
+
     event SubscribedEvent(address member);
 
-    constructor(address defaultPublisher) {        
+    constructor(address defaultPublisher, uint deposit) payable {
+        require(deposit == msg.value, "Deposit what you send");
         publisher = defaultPublisher;
-        InterfacePublisher(publisher).subscribe(msg.sender, 1337); 
+        InterfacePublisher(publisher).subscribe{value: msg.value}(payable(msg.sender), msg.value);
     }
 
     function publish(address member) external {
